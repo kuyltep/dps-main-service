58a4a3cccbdacb0560d4d02734eb358c0d75899e
Vlad Petlyuk <vlad10112005@inbox.ru>
2025-03-14 16:15:24 +0300

    delete useless files

--- src/core/decorators/user.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const User = createParamDecorator((data: string, context: ExecutionContext) => {
-  const request = context.switchToHttp().getRequest();
-  const user = request.user;
-
-  return data ? user?.[data] : user;
-});
