c3e796f365a8507ea4d9a475f47cac4bb8d8ebc3
Vlad Petlyuk <vlad10112005@inbox.ru>
2025-03-14 10:32:00 +0300

    update company and university queries dtos

--- src/common/dtos/company/query.company.dto.ts
+++ src/common/dtos/company/query.company.dto.ts
@@ -1,10 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { QueryPageDto } from '../query.page.dto';
-import { IsOptional, IsString } from 'class-validator';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { CompanyOrderByEnum } from 'src/common/enums/company.order-by.enum';
+import { OrderByTypeEnum } from 'src/common/enums/order-by.type.enum';
 
 export class QueryCompanyDto extends QueryPageDto {
   @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   name?: string;
+
+  @ApiProperty({ required: false, enum: CompanyOrderByEnum })
+  @IsOptional()
+  @IsEnum(CompanyOrderByEnum)
+  order_by: CompanyOrderByEnum = CompanyOrderByEnum.name;
+
+  @ApiProperty({ required: false, enum: OrderByTypeEnum })
+  @IsOptional()
+  @IsEnum(OrderByTypeEnum)
+  order: OrderByTypeEnum = OrderByTypeEnum.asc;
 }
--- src/common/dtos/university/query.university.dto.ts
+++ src/common/dtos/university/query.university.dto.ts
@@ -1,10 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString } from 'class-validator';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { QueryPageDto } from '../query.page.dto';
+import { UniversityOrderByEnum } from 'src/common/enums/university.order-by.enum';
+import { OrderByTypeEnum } from 'src/common/enums/order-by.type.enum';
 
 export class QueryGetUniversityDto extends QueryPageDto {
-  @ApiProperty()
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   name?: string;
+
+  @ApiProperty({ required: false, enum: UniversityOrderByEnum })
+  @IsOptional()
+  @IsEnum(UniversityOrderByEnum)
+  order_by: UniversityOrderByEnum;
+
+  @ApiProperty({ required: false, enum: OrderByTypeEnum })
+  @IsOptional()
+  @IsEnum(OrderByTypeEnum)
+  order: OrderByTypeEnum;
 }
