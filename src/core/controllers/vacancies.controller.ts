3bd613fc80762ef5e7ec0fc3408a43329f8d1ee3
Vlad Petlyuk <vlad10112005@inbox.ru>
2025-03-31 21:08:41 +0300

    update vacancies controller for find resumes

--- src/core/controllers/vacancies.controller.ts
+++ src/core/controllers/vacancies.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
-import { ApiExtraModels, ApiParam, ApiResponse, getSchemaPath } from '@nestjs/swagger';
+import { ApiExtraModels, ApiOperation, ApiParam, ApiResponse, getSchemaPath } from '@nestjs/swagger';
 import { CreateVacancyDto } from 'src/common/dtos/vacancy/create.vacancy.dto';
 import { QueryDeleteVacancyDto } from 'src/common/dtos/vacancy/delete.vacancy.dto';
 import { GetVacanciesDto, GetVacancyDto } from 'src/common/dtos/vacancy/get.vacancy.dto';
 import { QueryGetVacancyDto } from 'src/common/dtos/vacancy/query.vacancy.dto';
 import { UpdateVacancyDto } from 'src/common/dtos/vacancy/update.vacancy.dto';
 import { VacanciesService } from '../services/vacancies.service';
+import { GetResumesVectors, QueryGetResumesByVacancyDto } from 'src/common/dtos/qdrant/query.get.resumes.dto';
 
 @Controller('vacancies')
 @ApiExtraModels(GetVacanciesDto, GetVacancyDto, UpdateVacancyDto)
@@ -32,6 +33,14 @@ export class VacanciesController {
   @Get(':id')
   public async getVacancyById(@Param('id') id: string) {
     return await this.vacanciesService.getVacancyById(id);
+  }
+
+  @Get(':id/resumes')
+  @ApiParam({ name: 'id', type: String, required: true })
+  @ApiOperation({ summary: 'Get resumes for vacancy by id' })
+  @ApiResponse({ type: [GetResumesVectors] })
+  public async getResumesForVacancyById(@Param('id') id: string, @Query() query: QueryGetResumesByVacancyDto) {
+    return await this.vacanciesService.getResumesForVacancyById(id, query);
   }
 
   @ApiResponse({
