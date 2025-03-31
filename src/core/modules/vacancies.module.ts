3508b7028caedb3718ff93275f0c7d96f386aea7
Vlad Petlyuk <vlad10112005@inbox.ru>
2025-03-31 21:08:26 +0300

    update vacancies module logic

--- src/core/modules/vacancies.module.ts
+++ src/core/modules/vacancies.module.ts
@@ -1,10 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 import { VacanciesController } from '../controllers/vacancies.controller';
 import { VacanciesService } from '../services/vacancies.service';
+import { QdrantService } from '../services/qdrant.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Global()
 @Module({
+  imports: [HttpModule],
   controllers: [VacanciesController],
-  providers: [VacanciesService],
+  providers: [VacanciesService, QdrantService],
 })
 export class VacanciesModule {}
