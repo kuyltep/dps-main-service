de5731359db5062fba8caa187d374628052399b4
Vlad Petlyuk <vlad10112005@inbox.ru>
2025-03-31 21:07:43 +0300

    add interceptor

--- src/core/interceptors/errors.interceptor.ts
+++ src/core/interceptors/errors.interceptor.ts
@@ -1,16 +1,30 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler, HttpException } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  HttpException,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { logger } from 'src/common/utils/logger';
 
 @Injectable()
-export class ErorrsInterceptor implements NestInterceptor {
+export class ErrorsInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       catchError((error) => {
-        logger.error(JSON.stringify(error?.response ? error.response.data : error));
+        logger.error(JSON.stringify(error));
+
+        if (error instanceof HttpException) {
+          return throwError(() => error);
+        }
+
         if (error.response && error.response.data) {
-          throw new HttpException(error.response.data, error.response.data.status || error.response.status);
+          throw new HttpException(
+            error.response.data,
+            error.response.data.status || error.response.status,
+          );
         } else {
           throw new HttpException('Internal Server Error', 500);
         }
